4d9010cb901bff18fabaa6f9b1ca1d624ab3f6fc
Burak Tasci <burak.tasci@cozumtech.net>
2017-07-31 16:49:47 +0300

    build: mongoizer@1.0.0-rc.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,16 @@
 # Change Log
 All notable changes to this project will be documented in this file.
 
+## v1.0.0-rc.2 - 2017-07-31
+### Added
+- Added `findOptions` to find/search methods (closes [#4](https://github.com/fulls1z3/mongoizer/issue/4))
+
+### Fixed
+- Resolved `connection string` is checked after client initialization (closes [#3](https://github.com/fulls1z3/mongoizer/issue/3))
+- Resolved serialization issue from BsonType `ObjectId` to `string` (closes [#2](https://github.com/fulls1z3/mongoizer/issue/2))
+
+### Changed
+- Changed `collection` member access level to `public` (closes [#1](https://github.com/fulls1z3/mongoizer/issue/1))
+
 ## v1.0.0-beta.1 - 2017-07-13
 - Initial release
--- src/Mongoizer.Core/Properties/AssemblyInfo.cs
+++ src/Mongoizer.Core/Properties/AssemblyInfo.cs
@@ -1,14 +1,14 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly:AssemblyTitle("Mongoizer (domain classes)")]
-[assembly:AssemblyDescription("Shared domain classes for Mongoizer")]
-[assembly:AssemblyProduct("Mongoizer.Domain")]
+[assembly:AssemblyTitle("Mongoizer (core)")]
+[assembly:AssemblyDescription("Lightweight MongoDB repository implementation for .NET")]
+[assembly:AssemblyProduct("Mongoizer.Core")]
 [assembly:AssemblyCopyright("Copyright (c) 2017 Burak Tasci")]
 
 [assembly:ComVisible(false)]
 
 [assembly:Guid("46243641-cbf4-4ebb-a562-628c2aa2c690")]
 
-[assembly:AssemblyVersion("1.0.0.1")]
-[assembly:AssemblyFileVersion("1.0.0.1")]
+[assembly:AssemblyVersion("1.0.0.3")]
+[assembly:AssemblyFileVersion("1.0.0.3")]
